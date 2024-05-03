1e318c47bc7e0fce781126d0d59cfd70173bdbcf
seunguri <safg0421@gmail.com>
2024-05-03 14:36:42 +0900

    ✨ penguin model 추가 - RigidBody - tux, lowpoly model
    TODO: castShadow

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,14 +1,21 @@
+import { useGLTF } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
 import * as THREE from 'three'
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 
-const iceMaterial = new THREE.MeshStandardMaterial({ color: 'ice' })
+const iceMaterial = new THREE.MeshStandardMaterial({ color: "aqua" })
 
 function BlockStart({ position = [ 0, 0, 0 ] })
 {
+    const tux = useGLTF('./tux.glb')
+
     return <group position={ position }>
-        <mesh geometry={ boxGeometry } material={ iceMaterial } position={ [0, -0.1, 0] } scale={ [10, 0.2, 10] } receiveShadow>
-        </mesh>
+        <mesh geometry={ boxGeometry } material={ iceMaterial } position={ [0, -0.1, 0] } scale={ [10, 0.2, 10] } receiveShadow />
+
+        <RigidBody type={"fixed"} colliders={"hull"} position={ [0, 0.2, 0] } restitution={ 0.2 } friction={ 0 } >
+            <primitive object={ tux.scene } scale={ 0.01 } />
+        </RigidBody>
     </group>
 }
 
