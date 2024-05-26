b5336b1f3e80a936276d97861bbb66974de514a5
Me1e <mele0404@naver.com>
2024-05-26 15:42:15 +0900

    feat: emotion global style 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { css, Global } from '@emotion/react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          :root {
+            --max-width: 1100px;
+          }
+
+          * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+          }
+
+          html,
+          body {
+            max-width: 100vw;
+            overflow-x: hidden;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+
+          canvas {
+            height: 100vh;
+          }
+        `}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
