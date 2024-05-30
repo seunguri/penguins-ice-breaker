adf39f94f399b3fabf9035a743f3e1aa3909a323
Me1e <mele0404@naver.com>
2024-05-31 02:20:41 +0900

    fix: 불필요한 세미콜론 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ export default function App({ Component, pageProps }: AppProps) {
           }
         `}
       />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
